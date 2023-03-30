70e642434d14ef340fee023486f95829ec8f6595
boydlakers15 <jboyd115@icloud.com>
2023-03-30 18:40:49 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './comp/Home';
+import About from './comp/About';
+import Contact from './comp/Contact';
+import Recipes from './comp/Recipes';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <nav>
+        <NavLink to={"/Home"}>
+            Home
+        </NavLink>
+      </nav> */}
+    
+    <BrowserRouter>
+      <div>       
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/Recipes" element={<Recipes/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+// import logo from './logo.svg';
+// import React from 'react';
+// import './App.css';
+// import { BrowserRouter, Route, Routes} from 'react-router-dom';
+// import Home from './comp/Home';
+// import About from './comp/About';
+// import Contact from './comp/Contact';
+// import Navbar from './comp/Navbar';
+
+// function App() {
+//   return (
+//     <div>
+//       {/* <nav>
+//         <NavLink to={"/Home"}>
+//             Home
+//         </NavLink>
+//       </nav> */}
+    
+//     <BrowserRouter>
+//       <div>
+//         <Navbar />
+
+//         <Routes>
+//           <Route exact path="/Home" element={<Home />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/contact" element={<Contact />} />
+//         </Routes>
+//       </div>
+//     </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
